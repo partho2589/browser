a7575c3d95c7cdf2d159fc423046e1b77f9c122f
partho2589 <parthomalo615@.com>
2022-03-03 23:34:47 +0600

    added all files

---
+++ timeout.js
@@ -0,0 +1,10 @@
+
+function outline ()  {
+    console.log('My javascript coding')
+}
+
+console.log(111111)
+console.log(2222222)
+console.log(3333333 )
+setTimeout(outline, 3000)
+console.log(444444444444)
